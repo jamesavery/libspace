e1957860aee3df4ed13aff4f1944bc4dab15731f
James Avery <avery@diku.dk>
2009-09-15 16:40:09 +0200

    Deleted non-source files.

--- src/instances-grid.cc
+++
@@ -1,7 +0,0 @@
-// Instantiation of the most commonly used numbers of dimensions
-#include <space/grid/regulargrid.h>
-#include "grid/regulargrid.cc"
-
-template class RegularGrid<1>;
-template class RegularGrid<2>;
-template class RegularGrid<3>;
--- src/instances-interval.cc
+++
@@ -1,3 +0,0 @@
-#include <space/storage/interval.h>
-#include "storage/interval.cc"
-template class Intervals<unsigned int>;
